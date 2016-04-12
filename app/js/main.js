43d9bd201ebed43e36e7971723eb4b82ed92f538
Vadim Derman <thenoodly@gmail.com>
2016-04-11 21:58:07 -0700

    added rewiev form

--- app/js/main.js
+++ app/js/main.js
@@ -6,6 +6,8 @@
         
 
     function init() {
+        
+
         myMap = new ymaps.Map("map", {
             // center: [36.09922199, -115.12871905], 
             // zoom: 14
@@ -19,37 +21,15 @@
     function openForm(e) {
         var coords     = e.get('coords');
         var content = document.querySelector('.review-content');
-        
+
         if(content) {
             Ps.initialize(content);
         }
-        // var source     = $("#form-review").html();
-        // var templateFn = Handlebars.compile(source);
-        // var template   = templateFn({data: data});
-        // if (myPlacemark) {
-        //     myPlacemark.geometry.setCoordinates(coords);
-        // }
-        // else {
-        //     myPlacemark = createPlacemark(coords);
-        //     myMap.geoObjects.add(myPlacemark);
-        //     myPlacemark.events.add('dragend', function () {
-        //         getAddress(myPlacemark.geometry.getCoordinates());
-        //     });
-        // }
+
         getAddress(coords);
     }
 
-    // function createPlacemark(coords) {
-    //     return new ymaps.Placemark(coords, {
-    //         iconContent: 'поиск...'
-    //     }, {
-    //         preset: 'islands#violetStretchyIcon',
-    //         draggable: true
-    //     });
-    // }
-
     function getAddress(coords) {
-        // myPlacemark.properties.set('iconContent', 'поиск...');
         ymaps.geocode(coords).then(function (res) {
             var firstGeoObject = res.geoObjects.get(0);
             markData = firstGeoObject.properties.get('text');
@@ -63,12 +43,25 @@
         var templateFn = Handlebars.compile(source);
         var template   = templateFn({data: markData});
         var result     = document.querySelector('.review');
-        console.log(markData);
+
         result.innerHTML = template;
 
-        var content = document.querySelector('.review-content');
+        // scroll
+        addScroll();
 
+        var closeBtn = document.querySelector('.review-title__close');
+        closeBtn.addEventListener('click', closeRewiev);
+    }
+
+    function addScroll() {
+        var content = document.querySelector('.review-content');
         Ps.initialize(content);
+    }
+
+    function closeRewiev(e) {
+        var result     = document.querySelector('.review');
+
+        result.innerHTML = '';
     }
 
 }());
