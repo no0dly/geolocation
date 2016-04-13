275032d422ddaaa7f562d9ba91f0cd2e139909e9
Vadim Derman <thenoodly@gmail.com>
2016-04-13 02:30:23 -0700

    promise

--- app/js/main.js
+++ app/js/main.js
@@ -1,68 +1,70 @@
 (function() {
-    ymaps.ready(init);
-    var myMap,
-        myPlacemark,
-        markData;
-        
 
-    function init() {
-        
-
-        myMap = new ymaps.Map("map", {
+    new Promise(function(resolve) {
+        ymaps.ready(resolve);
+    }).then(function() {
+        return new Promise(function(resolve) {
+            myMap = new ymaps.Map("map", {
             // center: [36.09922199, -115.12871905], 
             // zoom: 14
-            center: [55.76, 37.64], 
-            zoom: 14
+                center: [55.76, 37.64], 
+                zoom: 14
+            });
+
+            resolve(myMap);
         });
+    }).then(function(val) {
+        val.events.add('click', openForm);
 
-        myMap.events.add('click', openForm);
-    }
 
-    function openForm(e) {
-        var coords     = e.get('coords');
-        var content = document.querySelector('.review-content');
+        function openForm(e) {
+            var coords  = e.get('coords');
+            var content = document.querySelector('.review-content');
+            
+            if(content) {
+                Ps.initialize(content);
+            }
 
-        if(content) {
-            Ps.initialize(content);
+            getAddress(coords);
         }
 
-        getAddress(coords);
-    }
+        function getAddress(coords) {
+            ymaps.geocode(coords).then(function (res) {
+                var firstGeoObject = res.geoObjects.get(0);
+                var markData = firstGeoObject.properties.get('text');
 
-    function getAddress(coords) {
-        ymaps.geocode(coords).then(function (res) {
-            var firstGeoObject = res.geoObjects.get(0);
-            markData = firstGeoObject.properties.get('text');
-        });
-        
-        drawForm(markData);
-    }
+               drawForm(markData);
+            });
+        }
 
-    function drawForm(markData) {
-        var source     = document.getElementById('form-review').innerHTML;
-        var templateFn = Handlebars.compile(source);
-        var template   = templateFn({data: markData});
-        var result     = document.querySelector('.review');
+        function drawForm(markData) {
+            new Promise(function(resolve) {
+                var source     = document.getElementById('form-review').innerHTML;
+                var templateFn = Handlebars.compile(source);
+                var template   = templateFn({data: markData});
+                var result     = document.querySelector('.review');
 
-        result.innerHTML = template;
+                result.innerHTML = template;
 
-        // scroll
-        addScroll();
+                resolve();
+            }).then(function() {
+                //add scroll
+                var content = document.querySelector('.review-content');
+                Ps.initialize(content);
 
-        var closeBtn = document.querySelector('.review-title__close');
-        closeBtn.addEventListener('click', closeRewiev);
-    }
+            }).then(function() {
+                //close btn
+                var closeBtn = document.querySelector('.review-title__close');
+                closeBtn.addEventListener('click', closeRewiev);
 
-    function addScroll() {
-        var content = document.querySelector('.review-content');
-        Ps.initialize(content);
-    }
+                function closeRewiev(e) {
+                    var result     = document.querySelector('.review');
+                    result.innerHTML = '';
+                }
+            });
+        }
 
-    function closeRewiev(e) {
-        var result     = document.querySelector('.review');
-
-        result.innerHTML = '';
-    }
+    });
 
 }());
 
